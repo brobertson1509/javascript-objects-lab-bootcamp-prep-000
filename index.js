711035eeb4a4d2fc627887170f70880c5e47a890
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 16:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ flour: '3 cups'
 }
 
 function updateObjectWithKeyAndValue(recipe, ingredient, quantity){
-  return Object.assign ({}, recipe, { butter: ["1 stick"] })
+  return Object.assign ({}, recipe, { butter: ["1 stick"] });
+  return Object.assign ({}, recipe, { flour: ["2 cups"] });
 }
