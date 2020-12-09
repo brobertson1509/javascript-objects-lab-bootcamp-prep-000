679b4f6132cc27e20b336be8816c00eae221de2e
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 17:48:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var obj = {}
 
 function updateObjectWithKeyAndValue(obj, key, value){
-  return Object.assign ({}, obj, { prop2: 2 });
+  return Object.assign ({}, obj, { [key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
