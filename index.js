6047c44b4cef083beb69ce7b16a65f9e454a8eb3
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 16:44:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var obj = {prop: 1
 }
 
-function updateObjectWithKeyAndValue(obj, prop, ){
+function updateObjectWithKeyAndValue(obj, prop, value){
   return Object.assign ({}, recipe, { butter: ["1 stick"] });
   return Object.assign ({}, recipe, { flour: ["2 cups"] });
 }
